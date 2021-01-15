c7ed8efc5bc7bfe2c2bff9fb90b025a2d8fe2036
Jozuah <galbas.joshua@gmail.com>
2021-01-15 15:39:42 +0100

    ajout de la parti erreur et de la partie qui remplit le mot

--- script.py
+++ script.py
@@ -26,6 +26,25 @@ logger.info("Start log")
 my_word='maison'
 error_count=0
 test_letter=0
+my_word_empty=[]
+
+def test_letter_function(user_letter,game_word):
+    for i in game_word :
+        if i == user_letter:
+            number = 1
+            logger.info("L'utilisateur a entré une lettre valide")
+            return number
+    number = 0
+    logger.info("L'utilisateur n'a pas entré une lettre valide")
+    return number
+
+my_word_as_list=list(my_word)
+b=0
+for b in range(len(my_word)):
+    my_word_empty.append('_')
+
+print(my_word_empty)
+
 
 #Affichage du mot vide
 i=0
@@ -35,21 +54,32 @@ for i in range(len(my_word)):
 logger.info("le mot à trouver est:")
 logger.info(my_word)
 
-def test_letter_function(user_letter,game_word):
-    for i in game_word :
-        if i == user_letter:
-            number = 1
-            return number
-    number = 0
-    return number
+
     
 
-while error_count <= 5 :
+while error_count < 5 :
 
-    my_letter = input("Entrer une lettre:")
+    my_letter = input("\nEntrer une lettre:")
     logger.info("la lettre de l'user est:")
     logger.info(my_letter)
 
     test_letter= test_letter_function(my_letter,my_word)
 
-    print(test_letter)
+    if(test_letter==0):
+        print(my_letter, " n'est pas une lettre du mot caché")
+        error_count = error_count + 1
+        logger.info("Nombre d'erreurs actuel")
+        logger.info(error_count)
+
+    elif(test_letter==1):
+        for compteur in range (len(my_word)):
+            if my_letter == my_word_as_list[compteur]:
+                my_word_empty[compteur]=my_word_as_list[compteur]
+        logger.info(my_word_empty)
+
+    my_word_empty_str=' '.join(map(str,my_word_empty))
+    print(my_word_empty_str)
+
+        
+
+print("jeu perdu")
