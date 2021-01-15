fb49bd7b187eb44e322ccf6afd7fbe0c9dcb7e61
Jozuah <galbas.joshua@gmail.com>
2021-01-15 22:29:36 +0100

    Ajout du pendu en ASCII

--- script.py
+++ script.py
@@ -2,6 +2,9 @@
 
 import logging
 
+
+
+
 from logging.handlers import RotatingFileHandler
  
 # création de l'objet logger qui va nous servir à écrire dans les logs
@@ -32,40 +35,87 @@ my_txt_file.close()
 
 logger.info("Start log")
 
-my_word='evidemment'
+
+from random_words import RandomWords
+rw = RandomWords()
+
+
+my_word = rw.random_word()
 error_count=0
 test_letter=0
 word_complete=0
+max_error=5
 my_word_empty=[]
 
+#Fonction qui teste si une lettre est contenue dans un mot
 def test_letter_function(user_letter,game_word):
     for i in game_word :
         if i == user_letter:
-            number = 1
             logger.info("L'utilisateur a entré une lettre valide")
-            return number
-    number = 0
+            return 1
     logger.info("L'utilisateur n'a pas entré une lettre valide")
-    return number
+    return 0
 
+#Fonction qui test si le mot a cherché est completement trouvé
 def test_full_word (my_current_list):       
     for item in my_current_list: 
-        cnt = 0
         if item == '_':
             logger.info("Le mot n'a pas été trouvé totalement")
-            cnt = 0
-            return cnt
-    cnt = 1
+            return 0
     logger.info("Le mot a été trouvé totalement")
-    return cnt
+    return 1
 
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+#Initialisation d'une liste obtenue a partir du mot à chercher
 my_word_as_list=list(my_word)
-b=0
-for b in range(len(my_word)):
+for i in range(len(my_word)):
     my_word_empty.append('_')
 
-#Affichage du mot vide
-i=0
+#Affichage du mot vide et du pendu
+print(HANGMANPICS[0])
 for i in range(len(my_word)):
     print("_ ", end='')
 
@@ -74,34 +124,51 @@ logger.info(my_word)
 
 
     
-
-while (error_count < 5) and (word_complete <1) :
+###LE JEU : BOUCLE QUI TOURNE TANT QUE L'UTILISATEUR N'A PAS FAIT 5 ERREURS
+### OU SI LE MOT N'A PAS ÉTÉ COMPLÈTEMENT TROUVÉ
+while (error_count < max_error) and (word_complete <1) :
     my_letter = input("\n\nEntrer une lettre:")
     logger.info("la lettre de l'user est:")
     logger.info(my_letter)
 
+    #Test pour savoir si la lettre de l'user est contenue dans le mot
     test_letter= test_letter_function(my_letter,my_word)
 
+    #Si NON : on rajoute une erreur et on affiche le pendu
     if(test_letter==0):       
         error_count = error_count + 1
-        print(my_letter, ": mauvais choix,  %s/5 erreurs" %(error_count))
+        print(my_letter, ": mauvais choix,  %s/%s erreurs" %(error_count,max_error))
         logger.info("Nombre d'erreurs actuel")
         logger.info(error_count)
 
+    #Si OUI : on ajoute la lettre au mot vide
     elif(test_letter==1):
         for compteur in range (len(my_word)):
             if my_letter == my_word_as_list[compteur]:
-                my_word_empty[compteur]=my_word_as_list[compteur]
+                my_word_empty[compteur] = my_word_as_list[compteur]
         logger.info(my_word_empty)
-
+    
+    #Test si le mot est complet
         word_complete = test_full_word(my_word_empty)
  
-
+    #Affichage du mot a chercher en tant que chaine de caracteres
     my_word_empty_str=' '.join(map(str,my_word_empty))
+    print(HANGMANPICS[error_count])
     print(my_word_empty_str)
 
 
-if error_count>= 5:
-    print("jeu perdu")
+
+if error_count>= max_error:
+    print("\nJeu perdu")
+    logger.info("Jeu perdu")
+    logger.info("nombre d'erreurs :")
+    logger.info(error_count) 
+    print("\nLe mot à trouver était :", my_word)  
 elif word_complete == 1 :
-    print("jeu gagné")
+    print("\nJeu gagné")
+    logger.info("Jeu gagné") 
+    logger.info("mot initial :")   
+    logger.info(my_word) 
+    logger.info("mot trouvé :")  
+    logger.info(my_word_empty_str)  
+logger.info("End log")
